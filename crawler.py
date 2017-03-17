e960583b6ba89efff9ba081c7fa6d8041fd6b653
bcoble <ben.j.coble@gmail.com>
2017-03-17 13:43:41 -0500

    Fix issue with split cards and script can now handle other mispelled cards

--- crawler.py
+++ crawler.py
@@ -14,7 +14,7 @@ def crawlWebPage():
 	page = requests.get(base_url + modern_url)
 	tree = html.fromstring(page.content)
 
-	archetypeTileXpath = '//div[2]/div/h2/span/a'
+	archetypeTileXpath = '//div[2]/div/h2/span[@class="deck-price-online"]/a'
 	tileNodes = sorted(set(tree.xpath(archetypeTileXpath)))
 	#print tileNodes[0].xpath('@href')
 	#print tileNodes[0].xpath('text()')
@@ -49,6 +49,7 @@ def readAndSaveDeckList(deckName, deckLink):
 				f.write('\nSideboard\n')
 		elif len(td) == 4:
 			card = getCardName(td)
+			f.write(card['count'] + ' ' + card['cardname'] + '\n')
 
 	f.close()
 
@@ -58,42 +59,44 @@ def getCardName(td):
 	card['count'] = td[0].text.strip()
 	if (len(td[1].xpath('a')) > 0):
 		card['cardname'] = td[1].xpath('a')[0].text
-	elif (td[1].text.strip() == 'Breaking' || td[1].text.strip() == 'Entering'):
+	elif (td[1].text.strip() == 'Breaking' or td[1].text.strip() == 'Entering'):
 		card['cardname'] = 'Breaking/Entering'
-	elif (td[1].text.strip() == 'Wear' || td[1].text.strip() == 'Tear'):
+	elif (td[1].text.strip() == 'Wear' or td[1].text.strip() == 'Tear'):
 		card['cardname'] = 'Wear/Tear'
-	elif (td[1].text.strip() == 'Catch' || td[1].text.strip() == 'Release'):
+	elif (td[1].text.strip() == 'Catch' or td[1].text.strip() == 'Release'):
 		card['cardname'] = 'Catch/Release'
-	elif (td[1].text.strip() == 'Alive' || td[1].text.strip() == 'Well'):
+	elif (td[1].text.strip() == 'Alive' or td[1].text.strip() == 'Well'):
 		card['cardname'] = 'Alive/Well'
-	elif (td[1].text.strip() == 'Armed' || td[1].text.strip() == 'Dangerous'):
+	elif (td[1].text.strip() == 'Armed' or td[1].text.strip() == 'Dangerous'):
 		card['cardname'] = 'Armed/Dangerous'
-	elif (td[1].text.strip() == 'Wear' || td[1].text.strip() == 'Tear'):
+	elif (td[1].text.strip() == 'Wear' or td[1].text.strip() == 'Tear'):
 		card['cardname'] = 'Wear/Tear'
-	elif (td[1].text.strip() == 'Far' || td[1].text.strip() == 'Away'):
+	elif (td[1].text.strip() == 'Far' or td[1].text.strip() == 'Away'):
 		card['cardname'] = 'Far/Away'
-	elif (td[1].text.strip() == 'Beck' || td[1].text.strip() == 'Call'):
+	elif (td[1].text.strip() == 'Beck' or td[1].text.strip() == 'Call'):
 		card['cardname'] = 'Beck/Call'
-	elif (td[1].text.strip() == 'Flesh' || td[1].text.strip() == 'Blood'):
+	elif (td[1].text.strip() == 'Flesh' or td[1].text.strip() == 'Blood'):
 		card['cardname'] = 'Flesh/Blood'
-	elif (td[1].text.strip() == 'Turn' || td[1].text.strip() == 'Burn'):
+	elif (td[1].text.strip() == 'Turn' or td[1].text.strip() == 'Burn'):
 		card['cardname'] = 'Turn/Burn'
-	elif (td[1].text.strip() == 'Down' || td[1].text.strip() == 'Dirty'):
+	elif (td[1].text.strip() == 'Down' or td[1].text.strip() == 'Dirty'):
 		card['cardname'] = 'Down/Dirty'
-	elif (td[1].text.strip() == 'Give' || td[1].text.strip() == 'Take'):
+	elif (td[1].text.strip() == 'Give' or td[1].text.strip() == 'Take'):
 		card['cardname'] = 'Give/Take'
-	elif (td[1].text.strip() == 'Profit' || td[1].text.strip() == 'Loss'):
+	elif (td[1].text.strip() == 'Profit' or td[1].text.strip() == 'Loss'):
 		card['cardname'] = 'Profit/Loss'
-	elif (td[1].text.strip() == 'Protect' || td[1].text.strip() == 'Serve'):
+	elif (td[1].text.strip() == 'Protect' or td[1].text.strip() == 'Serve'):
 		card['cardname'] = 'Protect/Serve'
-	elif (td[1].text.strip() == 'Ready' || td[1].text.strip() == 'Willing'):
+	elif (td[1].text.strip() == 'Ready' or td[1].text.strip() == 'Willing'):
 		card['cardname'] = 'Ready/Willing'
-	elif (td[1].text.strip() == 'Toil' || td[1].text.strip() == 'Trouble'):
+	elif (td[1].text.strip() == 'Toil' or td[1].text.strip() == 'Trouble'):
 		card['cardname'] = 'Toil/Trouble'
 
-	print card
-	f.write(card['count'] + ' ' + card['cardname'] + '\n')
+	elif (len(td[1].xpath('a')) == 0):
+		card['cardname'] = td[1].text.strip()
 
+	print card
+	return card
 
 def getModernDecks():
 	modern_decks = crawlWebPage()
